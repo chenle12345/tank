fc7ec230239878c67cfbb4c167c6092a0abe5a9a
chenle <1551863449@qq.com>
2020-09-15 22:40:36 +0800

    画出一个爆炸

---
+++ src/com/mashibing/tank/Explode.java
@@ -0,0 +1,39 @@
+package com.mashibing.tank;
+
+import java.awt.*;
+
+/**
+ * @Auther: chenle
+ * @Date: 2020/9/10 - 22:35
+ * @Description: com.mashibing.tank
+ * @version: 1.0
+ */
+public class Explode {
+
+    public static int WIDTH = ResourceMgr.explodes[0].getWidth();
+    public static int HEIGHT = ResourceMgr.explodes[0].getHeight();
+
+    private int x, y;
+
+    private boolean living = true;
+    private TankFrame tf = null;
+
+    private int step = 0;
+
+    public Explode(int x, int y, TankFrame tankFrame) {
+        this.x = x;
+        this.y = y;
+        this.tf = tankFrame;
+    }
+
+
+    public void paint(Graphics g) {
+        g.drawImage(ResourceMgr.explodes[step++], x, y, null);
+
+        if (step >= ResourceMgr.explodes.length) {
+            step = 0;
+        }
+
+    }
+
+}
