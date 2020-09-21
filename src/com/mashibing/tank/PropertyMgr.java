2bf9799e8eec7e650e31e4380756d324680b7d13
chenle <1551863449@qq.com>
2020-09-21 22:07:36 +0800

    加入配置文件，让程序更灵活

---
+++ src/com/mashibing/tank/PropertyMgr.java
@@ -0,0 +1,37 @@
+package com.mashibing.tank;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * @Auther: chenle
+ * @Date: 2020/9/21 - 21:40
+ * @Description: PACKAGE_NAME
+ * @version: 1.0
+ */
+public class PropertyMgr {
+
+    static Properties props = new Properties();
+
+    static {
+        try {
+            props.load(PropertyMgr.class.getClassLoader().getResourceAsStream("config"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static Object get(String key) {
+        if (props == null) {
+            return null;
+        } else {
+            return props.get(key);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(PropertyMgr.get("initTankCount"));
+    }
+
+}
